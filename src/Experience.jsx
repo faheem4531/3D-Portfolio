3d45816e1ba0dfca6e928a47e69d7fc3d16d70c8
faheem4531 <faheem4531@gmail.com>
2024-02-05 03:16:24 +0500

    load macbook modal

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,15 +1,19 @@
-import { OrbitControls } from '@react-three/drei'
+import { Center, Environment, Float, OrbitControls, useGLTF } from '@react-three/drei'
 
-export default function Experience()
-{
+export default function Experience() {
+    const macbook = useGLTF('./model.gltf')
+
     return <>
 
+        <Environment preset='city' />
         <OrbitControls makeDefault />
-        
-        <mesh>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
+        <color args={['#241a1a']} attach='background' />
 
+        <Float rotationIntensity={0.4}>
+            <primitive
+                object={macbook.scene}
+                position-y={-1.2}
+            />
+        </Float>
     </>
 }
