58ffea6b878114bb60b4ee6495351b3d03bdd014
faheem4531 <faheem4531@gmail.com>
2024-02-05 03:29:41 +0500

    limitize orbital controls with PresentationControls

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,4 +1,4 @@
-import { Center, Environment, Float, OrbitControls, useGLTF } from '@react-three/drei'
+import { Environment, Float, PresentationControls, useGLTF } from '@react-three/drei'
 
 export default function Experience() {
     const macbook = useGLTF('./model.gltf')
@@ -6,14 +6,20 @@ export default function Experience() {
     return <>
 
         <Environment preset='city' />
-        <OrbitControls makeDefault />
         <color args={['#241a1a']} attach='background' />
 
-        <Float rotationIntensity={0.4}>
-            <primitive
-                object={macbook.scene}
-                position-y={-1.2}
-            />
-        </Float>
+        <PresentationControls
+            global
+            rotation={[0.13, 0.1, 0]}
+            polar={[-0.4, 0.2]} // for vertical controls
+            azimuth={[-1, 1]} // fro horizontal controls
+        >
+            <Float rotationIntensity={0.4}>
+                <primitive
+                    object={macbook.scene}
+                    position-y={-1.2}
+                />
+            </Float>
+        </PresentationControls>
     </>
 }
